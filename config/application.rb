a00ec78433f88067c429ed491207387248d88c7f
Kouga766 <houkenkokushi@gmail.com>
2021-12-07 15:17:00 +0000

    日本時間変更

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ module Construction
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.i18n.default_locale = :ja
 
      config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
