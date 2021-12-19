92a20000c39f572f90a4d6450cfd9be9aa5746ed
Kouga766 <houkenkokushi@gmail.com>
2021-12-19 01:13:40 +0000

    production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,9 +18,8 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # public直下のファイルも読ませる
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
@@ -95,5 +94,6 @@ Rails.application.configure do
   config.action_cable.url = 'ws://35.74.30.14/cable'
   config.action_cable.allowed_request_origins = [ 'http://35.74.30.14', /http?:\/\/.*/, /https?:\/\/.*/ ]
   ActionCable.server.config.disable_request_forgery_protection = true
+
 
 end
